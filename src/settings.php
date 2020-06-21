0ec0192cf8b414cf797649167cbc789eaacb4ec3
Billy Anderson Matjik <daisukikancolle@gmail.com>
2020-06-21 21:47:17 +0700

    Connection Local

--- src/settings.php
+++ src/settings.php
@@ -16,7 +16,7 @@ return [
             'level' => \Monolog\Logger::DEBUG,
         ],
         'db' =>[
-            "host"=> 'mysql:unix_socket=/cloudsql/krisjaya-2020:asia-southeast2:krisjayadb',
+            "host"=> '34.101.179.169',
             "dbname"=> "kevindb",
             "user"=>"root",
             "pass"=>"sennek-bysfoc-1pArxo"
