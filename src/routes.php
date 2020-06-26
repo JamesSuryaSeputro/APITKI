17e76d5cb4d1d359b31a719dbfec8ea2723021dd
Billy Anderson Matjik <33697266+daisukikancolle@users.noreply.github.com>
2020-06-26 22:57:55 +0700

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -215,5 +215,160 @@ return function (App $app) {
             return $response->withJson(["status" => 0], 400);
         }
     });
+    
+    $app->post('/registertki', function ($request, $response) {
+        $username = $request -> getParam('username');
+        $password = $request -> getParam('password');
+        $nama = $request -> getParam('nama');
+        $nopassport = $request -> getParam('no_passport');
+        $noktp = $request -> getParam('no_ktp');
+        $tempatlahir = $request -> getParam('tempatlahir');
+        $tanggallahir = $request -> getParam('tanggallahir');
+        $umur = $request -> getParam('umur');
+        $kewarganegaraan = $request -> getParam('kewarganegaraan');
+        $jeniskelamin = $request -> getParam('jeniskelamin');
+        $alamat = $request -> getParam('alamat');
+        $notelp = $request -> getParam('no_telp');
+        $notelpalternative = $request -> getParam('no_telp_alternative');
+        $statuspernikahan = $request -> getParam('status_pernikahan');
+        $tinggibadan = $request -> getParam('tinggi_badan');
+        $beratbadan = $request -> getParam('berat_badan');
+        $matakiri = $request -> getParam('matakiri');
+        $matakanan = $request -> getParam('matakanan');
+        $butawarna = $request -> getParam('buta_warna');
+        $upahyangdiinginkan = $request -> getParam('upahyangdiinginkan');
+        $sektor1 = $request -> getParam('sektor1');
+        $sektor2 = $request -> getParam('sektor2');
+        $sektor3 = $request -> getParam('sektor3');
+        $pekerjaan1 = $request -> getParam('pekerjaan1');
+        $pekerjaan2 = $request -> getParam('pekerjaan2');
+        $pekerjaan3 = $request -> getParam('pekerjaan3');
+        $lokasi1 = $request -> getParam('lokasi1');
+        $lokasi2 = $request -> getParam('lokasi2');
+        $lokasi3 = $request -> getParam('lokasi3');
+        $pendidikanterakhir = $request -> getParam('pendidikanterakhir');
+        $bidang = $request -> getParam('bidang');
+        $mandarin = $request -> getParam('mandarin');
+        $inggris = $request -> getParam('inggris');
+        $bahasalain = $request -> getParam('bahasa_lain');
+        $sertifikatkerja1 = $request -> getParam('sertifikatkerja1');
+        $sertifikatkerja2 = $request -> getParam('sertifikatkerja2');
+        $sertifikatkerja3 = $request -> getParam('sertifikatkerja3');
+        $pengalamankerja1 = $request -> getParam('pengalamankerja1');
+        $pengalamankerjatanggalmulai1 = $request -> getParam('pengalamankerjatanggalmulai1');
+        $pengalamankerjaselesai1 = $request -> getParam('pengalamankerjaselesai1');
+        $pengalamankerja2 = $request -> getParam('pengalamankerja2');
+        $pengalamankerjatanggalmulai2 = $request -> getParam('pengalamankerjatanggalmulai2');
+        $pengalamankerjaselesai2 = $request -> getParam('pengalamankerjaselesai2');
+        $hasilmedicalcheckup = $request -> getParam('hasilmedicalcheckup');
+        $tanggalmedicalcheck = $request -> getParam('tanggalmedicalcheck');
+        $klinikmedicalcheck = $request -> getParam('klinikmedicalcheck');
+        $pendidikanbahasamandarin = $request -> getParam('pendidikanbahasamandarin');
+        $namalembagapendidikan = $request -> getParam('namalembagapendidikan');
+        $tglmulaipendidikanmandarin = $request -> getParam('tglmulaipendidikanmandarin');
+        $tglselesaipendidikanmandarin = $request -> getParam('tglselesaipendidikanmandarin');
+        $passfoto = $request -> getParam('passfoto');
+        $ttdfoto = $request -> getParam('ttdfoto');
+
+        $sth = $this->db->prepare("INSERT INTO tbl_user
+        (username, password, nama, no_passport, no_ktp, tempatlahir, tanggallahir, umur, kewarganegaraan, 
+        jeniskelamin, alamat, no_telp, no_telp_alternative, status_pernikahan, tinggi_badan, berat_badan, 
+        matakiri, matakanan, buta_warna, upahyangdiinginkan, sektor1, sektor2, sektor3, pekerjaan1, pekerjaan2, pekerjaan3,
+        lokasi1, lokasi2, lokasi3, pendidikanterakhir, bidang, mandarin, inggris, bahasa_lain,
+        sertifikatkerja1, sertifikatkerja2, sertifikatkerja3, pengalamankerja1, pengalamankerjatanggalmulai1,
+        pengalamankerjaselesai1, pengalamankerja2, pengalamankerjatanggalmulai2, pengalamankerjaselesai2,
+        hasilmedicalcheckup, tanggalmedicalcheck, klinikmedicalcheck, pendidikanbahasamandarin, namalembagapendidikan,
+        tglmulaipendidikanmandarin, tglselesaipendidikanmandarin, passfoto, ttdfoto)
+        VALUES (:username, :password, :nama, :no_passport, :no_ktp, :tempatlahir, :tanggallahir, :umur, :kewarganegaraan, :jeniskelamin,
+        :alamat, :no_telp, :no_telp_alternative, :status_pernikahan, :tinggi_badan, :berat_badan, :matakiri, :matakanan, :buta_warna,
+        :upahyangdiinginkan, :sektor1, :sektor2, :sektor3, :pekerjaan1, :pekerjaan2, :pekerjaan3, :lokasi1, :lokasi2, :lokasi3,
+        :pendidikanterakhir, :bidang, :mandarin, :inggris, :bahasa_lain, :sertifikatkerja1, :sertifikatkerja2, :sertifikatkerja3,
+        :pengalamankerja1, :pengalamankerjatanggalmulai1, :pengalamankerjaselesai1, :pengalamankerja2, :pengalamankerjatanggalmulai2,
+        :pengalamankerjaselesai2, :hasilmedicalcheckup, :tanggalmedicalcheck, :klinikmedicalcheck, :pendidikanbahasamandarin,
+        :namalembagapendidikan, :tglmulaipendidikanmandarin, :tglselesaipendidikanmandarin, :passfoto, :ttdfoto)");
+
+        $sth ->bindParam(':username',$username);
+        $sth ->bindParam(':password',$password);
+        $sth ->bindParam(':nama',$nama);
+        $sth ->bindParam(':no_passport',$nopassport);
+        $sth ->bindParam(':no_ktp',$noktp);
+        $sth ->bindParam(':tempatlahir',$tempatlahir);
+        $sth ->bindParam(':tanggallahir',$tanggallahir);
+        $sth ->bindParam(':umur',$umur);
+        $sth ->bindParam(':kewarganegaraan',$kewarganegaraan);
+        $sth ->bindParam(':jeniskelamin',$jeniskelamin);
+        $sth ->bindParam(':alamat',$alamat);
+        $sth ->bindParam(':no_telp',$notelp);
+        $sth ->bindParam(':no_telp_alternative',$notelpalternative);
+        $sth ->bindParam(':status_pernikahan',$statuspernikahan);
+        $sth ->bindParam(':tinggi_badan',$tinggibadan);
+        $sth ->bindParam(':berat_badan',$beratbadan);
+        $sth ->bindParam(':matakiri',$matakiri);
+        $sth ->bindParam(':matakanan',$matakanan);
+        $sth ->bindParam(':buta_warna',$butawarna);
+        $sth ->bindParam(':upahyangdiinginkan',$upahyangdiinginkan);
+        $sth ->bindParam(':sektor1',$sektor1);
+        $sth ->bindParam(':sektor2',$sektor2);
+        $sth ->bindParam(':sektor3',$sektor3);
+        $sth ->bindParam(':pekerjaan1',$pekerjaan1);
+        $sth ->bindParam(':pekerjaan2',$pekerjaan2);
+        $sth ->bindParam(':pekerjaan3',$pekerjaan3);
+        $sth ->bindParam(':lokasi1',$lokasi1);
+        $sth ->bindParam(':lokasi2',$lokasi2);
+        $sth ->bindParam(':lokasi3',$lokasi3);
+        $sth ->bindParam(':pendidikanterakhir',$pendidikanterakhir);
+        $sth ->bindParam(':bidang',$bidang);
+        $sth ->bindParam(':mandarin',$mandarin);
+        $sth ->bindParam(':inggris',$inggris);
+        $sth ->bindParam(':bahasa_lain',$bahasalain);
+        $sth ->bindParam(':sertifikatkerja1',$sertifikatkerja1);
+        $sth ->bindParam(':sertifikatkerja2',$sertifikatkerja2);
+        $sth ->bindParam(':sertifikatkerja3',$sertifikatkerja3);
+        $sth ->bindParam(':pengalamankerja1',$pengalamankerja1);
+        $sth ->bindParam(':pengalamankerjatanggalmulai1',$pengalamankerjatanggalmulai1);
+        $sth ->bindParam(':pengalamankerjaselesai1',$pengalamankerjaselesai1);
+        $sth ->bindParam(':pengalamankerja2',$pengalamankerja2);
+        $sth ->bindParam(':pengalamankerjatanggalmulai2',$pengalamankerjatanggalmulai2);
+        $sth ->bindParam(':pengalamankerjaselesai2',$pengalamankerjaselesai2);
+        $sth ->bindParam(':hasilmedicalcheckup',$hasilmedicalcheckup);
+        $sth ->bindParam(':tanggalmedicalcheck',$tanggalmedicalcheck);
+        $sth ->bindParam(':klinikmedicalcheck',$klinikmedicalcheck);
+        $sth ->bindParam(':pendidikanbahasamandarin',$pendidikanbahasamandarin);
+        $sth ->bindParam(':namalembagapendidikan',$namalembagapendidikan);
+        $sth ->bindParam(':tglmulaipendidikanmandarin',$tglmulaipendidikanmandarin);
+        $sth ->bindParam(':tglselesaipendidikanmandarin',$tglselesaipendidikanmandarin);
+        $sth ->bindParam(':passfoto',$passfoto);
+        $sth ->bindParam(':ttdfoto',$ttdfoto);
+        if($sth->execute()){
+            return $response->withJson(["status" => 1], 200);
+        }    else{
+            return $response->withJson(["status" => 0], 400);
+        }
+    });
+
+    $app->post('/registerpelatih', function ($request, $response) {
+        $username = $request -> getParam('username');
+        $password = $request -> getParam('password');
+        $nama_pelatih = $request -> getParam('nama_pelatih');
+        $sth = $this->db->prepare("INSERT INTO tabel_pelatih (nama_pelatih, username, password) VALUES (:nama_pelatih, :username, :password)");
+        if($sth->execute()){
+            return $response->withJson(["status" => 1], 200);
+        }    else{
+            return $response->withJson(["status" => 0], 400);
+        }
+    });
+
+    $app->post('/registerpegawai', function ($request, $response) {
+        $username = $request -> getParam('username');
+        $password = $request -> getParam('password');
+        $nama_pelatih = $request -> getParam('nama_pegawai');
+        $nip = $request -> getParam('nip');
+        $sth = $this->db->prepare("INSERT INTO tabel_pegawai (nama, username, password, nip) VALUES (:nama_pelatih, :username, :password, :nip)");
+        if($sth->execute()){
+            return $response->withJson(["status" => 1], 200);
+        }    else{
+            return $response->withJson(["status" => 0], 400);
+        }
+    });
 
 };
